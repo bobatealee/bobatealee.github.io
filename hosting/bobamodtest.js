e025d6a7c9dfd9ae2f3ba5c24d56c454147e61a8
Boba <44716190+bobatealee@users.noreply.github.com>
2021-02-15 15:05:17 -0600

    Update bobamodtest.js

--- hosting/bobamodtest.js
+++ hosting/bobamodtest.js
@@ -1,3 +1,5 @@
-window.onload = function myFunction() {
+function myFunction() {
   document.getElementsByTagName('body')[0].style = 'image-rendering: crisp-edges';
 }
+
+ window.onload = myFunction;
