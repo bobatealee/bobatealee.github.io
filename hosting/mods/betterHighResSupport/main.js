fd378eb7c6f1f3ec2bd236e84f78a7382007fad9
Boba <44716190+bobatealee@users.noreply.github.com>
2022-03-13 00:33:53 -0600

    small hosting change

--- hosting/mods/betterHighResSupport/main.js
+++ hosting/mods/betterHighResSupport/main.js
@@ -1,7 +1,9 @@
-Game.registerMod("better4ksupport",{
+Game.registerMod("betterHighResSupport",{
 	init:function(){
 		function makeCrispy() {
+			// make the whole game crispy!
 			document.getElementsByTagName('body')[0].style = 'image-rendering: pixelated; image-rendering: crisp-edges';
+			// keep buff icons at the top right the same because they have weird scaling
 			document.getElementById('buffs').style = 'image-rendering: auto';
 		}
 
