5c12abda01ea371e2f2f1751f51477bf81e6248b
Boba <44716190+bobatealee@users.noreply.github.com>
2022-04-11 01:46:49 -0500

    sesame

---
+++ hosting/mods/sesame/main.js
@@ -0,0 +1,27 @@
+if (sesame === undefined) var sesame = {};
+Game.registerMod("sesame",{
+	init:function(){
+		sesame.version = "1.0";
+
+		Game.registerHook("check", sesameCheck);
+
+		l('versionNumber').insertAdjacentHTML('beforeend','<a style="position:relative;left:4px;top:7px;overflow:hidden;z-index:10;" class="option neato" id="sesamereload">Reload</a>');
+		l('versionNumber').insertAdjacentHTML('beforeend','<a style="position:relative;left:4px;top:7px;overflow:hidden;z-index:10;" class="option neato" id="sesamedebug">Debug</a>');
+
+		AddEvent(l('sesamereload'),'click',function(){
+			Game.toReload=true;
+		});
+
+		AddEvent(l('sesamedebug'),'click',function(){
+			PlaySound('snd/tick.mp3');
+			Game.OpenSesame();
+			l('sesamedebug').style.display='none';
+		});
+
+		function sesameCheck() {
+			if (!Game.HasAchiev("Cheated cookies taste awful")) Game.Achievements['Cheated cookies taste awful'].won=1; // reminding cheaters of their sins
+		}
+		
+		Game.Notify("Sesame loaded!", "Version "+sesame.version, [10, 6], 2, 1);
+	}
+});
