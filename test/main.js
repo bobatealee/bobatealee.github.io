55b3e769a73ade476bf9befef1b2dee4a49846d0
Boba <44716190+bobatealee@users.noreply.github.com>
2022-08-30 23:16:43 -0500

    205

--- test/main.js
+++ test/main.js
@@ -7,12 +7,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
 				
 // title toy 1
 function title1() {
+	return // remove when toys are done
 	document.getElementById("title").classList.toggle("rainbow");
 };
 			
 // title toy 9
 function title9() {
+	return // remove when toys are done
 	document.getElementById("content").classList.toggle("flipped");
-	document.getElementById("modal").classList.toggle("flipped");
 	document.getElementById("html").classList.toggle("flippedColor");
 };
