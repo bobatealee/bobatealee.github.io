e0b4407b27510c7939b05fc9afd6bdd55bbdb7e3
Boba <44716190+bobatealee@users.noreply.github.com>
2022-09-13 00:23:22 -0500

    207

--- test/main.js
+++ test/main.js
@@ -3,17 +3,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	$(function() {
 		$('.titleOutline').attr('temp', function() {return $(this).html();});
 	});
+
+	for (let i = 0; i < document.getElementsByClassName("desktopButton").length; i++) {
+	  document.getElementsByClassName("desktopButton")[i].style.backgroundPosition = -(i*32)+"px 0px";
+	}
 });
 				
 // title toy 1
 function title1() {
-	return // remove when toys are done
 	document.getElementById("title").classList.toggle("rainbow");
 };
 			
 // title toy 9
 function title9() {
-	return // remove when toys are done
 	document.getElementById("content").classList.toggle("flipped");
 	document.getElementById("html").classList.toggle("flippedColor");
+};
+
+function textboxToggle(x) {
+	document.getElementById("textbox"+x).classList.toggle("hidden");
+	document.getElementById("desktopButton"+x).classList.toggle("hidden");
 };
